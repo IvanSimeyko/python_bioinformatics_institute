47434d279c5a5172da3c1dc5256d3f2d4bf61c58
IvanSimeyko <iv.simeyko@gmail.com>
2016-04-16 21:33:38 +0300

    solve function task

--- module_1/functions_task.py
+++ module_1/functions_task.py
@@ -1,8 +1,22 @@
 a = []
-
 def foo(arg1, arg2):
   a.append("foo")
 
 foo(a.append("arg1"), a.append("arg2"))
+#print(a)
 
-print(a)
+
+def closest_mod_5(x):
+    """
+    x - целое число
+    возвращаем число которое:
+        больше или равно x
+        делится нацело на 5
+    """
+    if x % 5 == 0:
+         return x
+    for i in range(1,5):
+        if (i+x) % 5 == 0:
+            return i+x
+
+x = closest_mod_5(13); print(x)

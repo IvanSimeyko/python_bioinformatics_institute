74961cace7ce9852da64292ecb7d9335a4079137
IvanSimeyko <iv.simeyko@gmail.com>
2016-04-17 21:27:51 +0300

    solved second task about classes

--- module_1/classes_tasks.py
+++ module_1/classes_tasks.py
@@ -16,8 +16,40 @@ class MoneyBox:
         self.value += v
         return self.value
 
-x = MoneyBox(10)
-x.add(7)
-print(x.value)
-print(x.add(1))
-print(x.value)
+# x = MoneyBox(10)
+# x.add(7)
+# print(x.value)
+# print(x.add(1))
+# print(x.value)
+
+class Buffer:
+    def __init__(self):
+        # конструктор без аргументов
+        self.val = []
+
+    def summa(self, val):
+        while len(val) >= 5:
+            x = sum(val[:5])
+            print(x)
+            del(val[:5])
+
+    def add(self, *a):
+        # добавить следующую часть последовательности
+        self.val.extend(a)
+        if len(self.val) >= 5:
+            self. summa(self.val)
+
+    def get_current_part(self):
+        # вернуть сохраненные в текущий момент элементы последовательности в порядке, в котором они были
+        # добавлены
+        return self.val
+
+buf = Buffer()
+buf.add(1, 2, 3)
+print(buf.get_current_part()) # вернуть [1, 2, 3]
+buf.add(4, 5, 6) # print(15) – вывод суммы первой пятерки элементов
+print(buf.get_current_part()) # вернуть [6]
+buf.add(7, 8, 9, 10) # print(40) – вывод суммы второй пятерки элементов
+print(buf.get_current_part()) # вернуть []
+buf.add(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1) # print(5), print(5) – вывод сумм третьей и четвертой пятерки
+print(buf.get_current_part()) # вернуть [1]

aa81411a3dbdc1141eab571dd6d2e68894566d7f
IvanSimeyko <iv.simeyko@gmail.com>
2016-04-17 09:55:50 +0300

    solved last task in lesson 1.3

--- module_1/functions_task.py
+++ module_1/functions_task.py
@@ -31,5 +31,16 @@ def fib(x):
         return 1
     else:
         return fib(x-1) + fib(x-2)
+#print(fib(7))
 
-print(fib(7))
+# решение последней задачи в 1.3
+n, k = map(int, input().split())
+def num_soch(n, k):
+    if k == 0:
+        return 1
+    elif k > n:
+        return 0
+    else:
+        f = num_soch(n-1, k) + num_soch(n-1, k-1)
+        return f
+print(num_soch(n, k))

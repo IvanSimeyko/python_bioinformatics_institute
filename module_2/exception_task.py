a6ae8a4c4f8db8e1392671e5362a86e58ae4e86e
IvanSimeyko <iv.simeyko@gmail.com>
2016-04-21 17:05:09 +0300

    solved first task

---
+++ module_2/exception_task.py
@@ -0,0 +1,10 @@
+def foo():
+    pass
+try:
+    foo()
+except ZeroDivisionError:
+    print("ZeroDivisionError ")
+except ArithmeticError:
+    print("ArithmeticError")
+except AssertionError:
+    print("AssertionError")

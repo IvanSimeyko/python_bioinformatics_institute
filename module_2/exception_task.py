887231f3639ee647fef6c295d66978e046b0a125
IvanSimeyko <iv.simeyko@gmail.com>
2016-04-21 17:27:48 +0300

    solved second task

--- module_2/exception_task.py
+++ module_2/exception_task.py
@@ -8,3 +8,21 @@ except ArithmeticError:
     print("ArithmeticError")
 except AssertionError:
     print("AssertionError")
+
+class NonPositiveError(Exception):
+    pass
+
+class PositiveList(list):
+
+    def append(self, p_object):
+        if p_object > 0:
+             x = super(PositiveList, self).append(p_object)
+             return x
+        elif p_object <= 0:
+            raise NonPositiveError(str(p_object) + ' is not positive number')
+
+a = PositiveList([1,2,3])
+a.append(4)
+print(a)
+a.append(-4)
+

4dff3e344c33ebd996a8deb3fcc9bf0bf54c038b
Ivan Simeyko <iv.simeyko@gmail.com>
2016-04-25 21:52:42 +0300

    added iterators_task.py

---
+++ module_2/iterators_task.py
@@ -0,0 +1,66 @@
+# class multifilter:
+#
+#     def __init__(self, iterable, *funcs, judge=judge_any):
+#         # iterable - исходная последовательность
+#         iterable = self
+#         # funcs - допускающие функции
+#
+#         # judge - решающая функция
+#         pass
+#
+#     def __iter__(self):
+#         # возвращает итератор по результирующей последовательности
+#         pass
+#
+#     def judge_half(pos, neg):
+#         # допускает элемент, если его допускает хотя бы половина фукнций (pos >= neg.)
+#         pass
+#
+#     def judge_any(pos, neg):
+#         # допускает элемент, если его допускает хотя бы одна функция (pos >= 1)
+#         pass
+#
+#     def judge_all(pos, neg):
+#         # допускает элемент, если его допускают все функции (neg == 0)
+#         pass
+#
+# def mul2(x):
+#     return x % 2 == 0
+#
+# def mul3(x):
+#     return x % 3 == 0
+#
+# def mul5(x):
+#     return x % 5 == 0
+#
+#
+# a = [i for i in range(31)] # [0, 1, 2, ... , 30]
+#
+# #print(list(multifilter(a, mul2, mul3, mul5)))
+# # [0, 2, 3, 4, 5, 6, 8, 9, 10, 12, 14, 15, 16, 18, 20, 21, 22, 24, 25, 26, 27, 28, 30]
+
+import itertools
+ans = []
+def is_simple(n):
+    d = n - 1
+    while d > 1:
+        if n % d == 0:
+            return False
+        d -= 1
+    return True
+
+def primes():
+    x = 2
+    while True:
+        if is_simple(x):
+            ans.append(x)
+            x += 1
+        else:
+            x += 1
+            continue
+    yield ans
+
+
+# a = int(input('enter number: '))
+# b = list(range(2, a+1))
+print(list(itertools.takewhile(lambda x: x <= 5, primes())))

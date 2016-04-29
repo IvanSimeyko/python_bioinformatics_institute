0bded254622b9924153200b1a4b371b9bbf101a0
Ivan Simeyko <iv.simeyko@gmail.com>
2016-04-29 23:16:32 +0300

    iterator task

--- module_2/iterators_task.py
+++ module_2/iterators_task.py
@@ -24,46 +24,46 @@ class DoubleElementListIterator:
 # print(next(x))
 
 
-# class multifilter:
-#
-#     def __init__(self, iterable, *funcs, judge=judge_any):
-#         # iterable - исходная последовательность
-#         iterable = self
-#         # funcs - допускающие функции
-#
-#         # judge - решающая функция
-#         pass
-#
-#     def __iter__(self):
-#         # возвращает итератор по результирующей последовательности
-#         pass
-#
-#     def judge_half(pos, neg):
-#         # допускает элемент, если его допускает хотя бы половина фукнций (pos >= neg.)
-#         pass
-#
-#     def judge_any(pos, neg):
-#         # допускает элемент, если его допускает хотя бы одна функция (pos >= 1)
-#         pass
-#
-#     def judge_all(pos, neg):
-#         # допускает элемент, если его допускают все функции (neg == 0)
-#         pass
-#
-# def mul2(x):
-#     return x % 2 == 0
-#
-# def mul3(x):
-#     return x % 3 == 0
-#
-# def mul5(x):
-#     return x % 5 == 0
-#
-#
-# a = [i for i in range(31)] # [0, 1, 2, ... , 30]
-#
-# #print(list(multifilter(a, mul2, mul3, mul5)))
-# # [0, 2, 3, 4, 5, 6, 8, 9, 10, 12, 14, 15, 16, 18, 20, 21, 22, 24, 25, 26, 27, 28, 30]
+class multifilter:
+
+    def __init__(self, iterable, *funcs, judge=judge_any):
+        # iterable - исходная последовательность
+        self.iterable = iterable
+        # funcs - допускающие функции
+        self.funcs = funcs
+        # judge - решающая функция
+        self.judge = judge
+
+    def __iter__(self):
+        return self
+
+
+    def judge_half(pos, neg):
+        # допускает элемент, если его допускает хотя бы половина фукнций (pos >= neg.)
+        pass
+
+    def judge_any(pos, neg):
+        # допускает элемент, если его допускает хотя бы одна функция (pos >= 1)
+        pass
+
+    def judge_all(pos, neg):
+        # допускает элемент, если его допускают все функции (neg == 0)
+        pass
+
+def mul2(x):
+    return x % 2 == 0
+
+def mul3(x):
+    return x % 3 == 0
+
+def mul5(x):
+    return x % 5 == 0
+
+
+a = [i for i in range(31)] # [0, 1, 2, ... , 30]
+
+#print(list(multifilter(a, mul2, mul3, mul5)))
+# [0, 2, 3, 4, 5, 6, 8, 9, 10, 12, 14, 15, 16, 18, 20, 21, 22, 24, 25, 26, 27, 28, 30]
 
 import itertools
 ans = []
@@ -85,4 +85,4 @@ def primes():
             x += 1
             continue
 
-print(list(itertools.takewhile(lambda x: x <= 31, primes())))
+#print(list(itertools.takewhile(lambda x: x <= 31, primes())))

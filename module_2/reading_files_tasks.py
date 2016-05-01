c71ccdf429a57ac373dbb10eca7595d647363f8e
Ivan Simeyko <iv.simeyko@gmail.com>
2016-05-01 14:06:57 +0300

    solved task 2

--- module_2/reading_files_tasks.py
+++ module_2/reading_files_tasks.py
@@ -1,3 +1,5 @@
+import os
+
 def record_revers(file1, file2):
     """
     считываем файл и записываем информацию в другой в обратном порядке
@@ -12,3 +14,16 @@ def record_revers(file1, file2):
 
 #record_revers('test.txt', 'answ.txt')
 
+def find_py(folder):
+    """
+    функция имет файлы .py в указанной папке и глубже и возвращает пути где они лежат
+    """
+    answ = []
+    for current_dir, dirs, files in os.walk(folder):
+        #print(current_dir, dirs, files)
+        for file in files:
+            if file.endswith('.py') and current_dir not in answ:
+                answ.append(current_dir)
+    return answ
+
+#print(find_py('sample'))

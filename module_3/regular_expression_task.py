e610cd229cb2caf04b9ff75d27ee89ebd9134e05
Ivan Simeyko <iv.simeyko@gmail.com>
2016-05-07 21:38:47 +0300

    solved five task

--- module_3/regular_expression_task.py
+++ module_3/regular_expression_task.py
@@ -1,46 +1,105 @@
 import re
+#
+# # video examples
+# pattern = r'abc'
+# string = r'dabc'
+# match_object = re.search(pattern, string)
+# print('match_object=', match_object)
+#
+# pattern = r'a[a-zA-Z]c'  # любая буква
+# string = r'abc'
+# match_object = re.match(pattern, string)
+# print('match_object= ', match_object)
+#
+# string = 'abc, acc, bbc'
+# all_inclusions = re.findall(pattern, string)
+# print('all_inclusions= ', all_inclusions)
+#
+# fixed_typos = re.sub(pattern, 'abc', string)
+# print('fixed_typos= ', fixed_typos)
+#
+# #example with interrogatory(?)
+# pattern = r'english\?'
+# string = r'Do you speak english?'
+# match_object = re.search(pattern, string)
+# print('match_object= ', match_object)
+#
+#
+# #example with ( )
+# pattern = r'(test|text)*'
+# string = r'testtext'
+# test = re.match(pattern, string)
+# print('test= ', test)
+#
+# #example with groups
+# pattern = r'Hello (abc|test)'
+# string = r'Hello abc'
+# test = re.match(pattern, string)
+# print('test= ', test)
+# print(test.group())    # 0 задан аргументом по умолчанию
+#
+# #example with groups
+# pattern = r'(\w*)-\1'    # \1 - номер группы
+# string = r'test-test'
+# test = re.match(pattern, string)
+# print('test= ', test)
+# print(test.group())    # 0 задан аргументом по умолчанию
+#
+# x = (re.match(r'text', 'TEXT', re.IGNORECASE))
+# print('x= ', x)
 
-# video examples
-pattern = r'abc'
-string = r'dabc'
-match_object = re.search(pattern, string)
-print('match_object=', match_object)
 
-pattern = r'a[a-zA-Z]c'  # любая буква
-string = r'abc'
-match_object = re.match(pattern, string)
-print('match_object= ', match_object)
-
-string = 'abc, acc, bbc'
-all_inclusions = re.findall(pattern, string)
-print('all_inclusions= ', all_inclusions)
-
-fixed_typos = re.sub(pattern, 'abc', string)
-print('fixed_typos= ', fixed_typos)
-
-#example with interrogatory(?)
-pattern = r'english\?'
-string = r'Do you speak english?'
-match_object = re.search(pattern, string)
-print('match_object= ', match_object)
+import sys
+sys.stdin = open("input.txt", "r")
+pattern = 'cat'
 
 
-#example with ( )
-pattern = r'(test|text)*'
-string = r'testtext'
-test = re.match(pattern, string)
-print('test= ', test)
+def first():
+    for line in sys.stdin:
+        line = line.rstrip()
+        sear = re.findall(pattern, line)
+        if len(sear) > 1:
+            print(line)
+#first()
 
-#example with groups
-pattern = r'Hello (abc|test)'
-string = r'Hello abc'
-test = re.match(pattern, string)
-print('test= ', test)
-print(test.group())    # 0 задан аргументом по умолчанию
 
-#example with groups
-pattern = r'(\w*)-\1'    # \1 - номер группы
-string = r'test-test'
-test = re.match(pattern, string)
-print('test= ', test)
-print(test.group())    # 0 задан аргументом по умолчанию
+def second():
+    for line in sys.stdin:
+        line = line.rstrip()
+        pattern = r'\bcat\b'
+        x = re.findall(pattern, line)
+        if x:
+            print(line)
+# second()
+
+
+def third():
+    for line in sys.stdin:
+        line = line.rstrip()
+        pattern = r'z[a-zA-Z]{3}z'   # [a-zA-Z]{2} means two letters
+        x = re.search(pattern, line)
+        if x:
+            print(line)
+
+# third()
+
+
+def forth():
+    for line in sys.stdin:
+        line = line.rstrip()
+        pattern = r'\\'
+        x = re.search(pattern, line)
+        if x:
+            print(line)
+
+#forth()
+
+def fifth():
+    for line in sys.stdin:
+        line = line.rstrip()
+        pattern = r"\b(\w+)\1\b"
+        x = re.search(pattern, line)
+        if x:
+            print(line)
+
+fifth()

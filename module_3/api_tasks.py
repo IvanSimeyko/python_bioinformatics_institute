76a19dcd55f9f8bcb7deed7b691d798abd0b9934
Ivan Simeyko <iv.simeyko@gmail.com>
2016-05-10 07:31:15 +0300

    start second api task

--- module_3/api_tasks.py
+++ module_3/api_tasks.py
@@ -41,6 +41,10 @@ def number_true_task():
 
 # second task. Example working with developers.artsy.net
 
+sys.stdin = open("input.txt", "r")
+res = {}
+
+
 def developers_artsy_net():
     client_id = '0c6388d1676572a87901'
     client_secret = '4dfc52d408b9d587bee6ef46fff1f86d'
@@ -57,3 +61,32 @@ def developers_artsy_net():
 
     # достаем токен
     token = j["token"]
+
+    # создаем заголовок, содержащий наш токен
+    headers = {"X-Xapp-Token": token}
+
+    for req in sys.stdin:
+        api_url = "https://api.artsy.net/api/artists/" + req.rstrip()   # delete \n in end string
+
+        # инициируем запрос с заголовком
+        r = requests.get(api_url, headers=headers)
+
+        # разбираем ответ сервера
+        j = json.loads(r.text)
+        if j['birthday'] not in res:
+            year = int(j['birthday'])
+            res[year] = []
+            res[year].append(j['sortable_name'])
+        elif j['birthday'] in res:
+            year = int(j['birthday'])
+            res[year].append(j['sortable_name'])
+
+        print(res)
+    # sort and output answer
+    for key, value in sorted(res.items()):
+        if len(value) > 1:
+                value.sort()
+        for i in value:
+            print(i)
+
+developers_artsy_net()
